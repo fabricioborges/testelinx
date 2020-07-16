89aeb44f7adc7fb0cd6086ef7cc379e96bae7709
Fabricio <fabricio.riff2@gmail.com>
2020-07-16 15:25:08 -0300

    alterado estrutura das pastas do projeto

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const ProductController = require('./app/controllers/ProductController');
-
-const routes = Router();
-
-routes.post('/products', ProductController.store);
-
-module.exports = routes;
